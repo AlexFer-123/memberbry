283e17cda8051b9a64232c772f5a5dbccb2b6ba8
Alexandre <Alexandrefer1231@gmail.com>
2023-10-26 23:29:10 -0300

    added common: route created

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './global.css'
 
-createApp(App).mount('#app')
+import Home from './views/HomePage.vue'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Home,
+      name: 'HomePage',
+    },
+  ],
+})
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
