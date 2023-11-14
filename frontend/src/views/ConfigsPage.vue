3f1eb370fb1d806ce94a21fd759dfc312050652a
Alexandre <Alexandrefer1231@gmail.com>
2023-11-14 10:37:11 -0300

    install yarn

--- frontend/src/views/ConfigsPage.vue
+++ frontend/src/views/ConfigsPage.vue
@@ -10,44 +10,16 @@
       </div>
     </div>
 
-    <div class="integrations flex flex-wrap">
+    <div class="integrations flex text-center flex-wrap">
       <div v-for="integration in integrations" :key="integration" class="">
-        <div class="card w-64 flex-wrap flex justify-center">
+        <div class="card w-64 flex-wrap flex-column gap-6 justify-center">
           <div class="flex" >
             <span class=" block subtitle font-medium">
               {{integration.name}}
             </span>
           </div>
 
-          <div class="sm:col-span-3 mt-2">
-            <label for="last-name" class="block text-sm font-light leading-6 text-gray-900">Nome</label>
-            <div class="mt-1">
-              <input v-model="authBody.name" type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-          
-          <div class="sm:col-span-3 mt-2">
-            <label for="last-name" class="block text-sm font-light leading-6 text-gray-900">URL Callback</label>
-            <div class="mt-1">
-              <input v-model="authBody.url_callback" type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-3 mt-2">
-            <label for="last-name" class="block text-sm font-light leading-6 text-gray-900">URL Website</label>
-            <div class="mt-1">
-              <input v-model="authBody.url_website" type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <div class="sm:col-span-3 mt-2">
-            <label for="last-name" class="block text-sm font-light leading-6 text-gray-900">Logo</label>
-            <div class="mt-1">
-              <input v-model="authBody.logo" type="text" name="surname" id="surname" autocomplete="family-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-            </div>
-          </div>
-
-          <button @click.prevent="auth2Panda()"> Auth</button>
+          <a :href="authLink" @click="auth2Panda()" class="py-2 px-6 bg-slate-800 rounded-md text-white mt-2" target="_blank">Auth</a>
         
         </div>
       </div>
@@ -59,23 +31,26 @@
 <script setup>
 import { ref } from "vue";
 
-const authBody = ref({})
-
 const integrations = ref([
   {
     name: 'Panda Video'
-  },
-  {
-    name: 'Vimeo'
-  },
-  {
-    name: 'Youtube'
   }
 ])
 
+const oAuthInfos = ref(
+  {
+    client_id: "1gojsiuasqum4srpcqu0c036gr",
+    scope: "aws.cognito.signin.user.admin",
+    redirect_uri: "http://localhost:8080/configs"
+  }
+)
+const authLink = ref(false)
+
 const auth2Panda = () => {
-  console.log(authBody.value)
+  authLink.value = `https://auth.pandavideo.com.br/oauth2/authorize?client_id=${oAuthInfos.value.client_id}&response_type=code&scope=${oAuthInfos.value.scope}&redirect_uri=${oAuthInfos.value.redirect_uri}`
+
 }
+
 </script>
 
 <style lang="scss" scoped>
