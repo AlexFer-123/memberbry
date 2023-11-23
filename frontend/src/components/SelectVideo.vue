0b3e507e2c864a6108c887c2411a2a0fe654cacb
Alexandre <Alexandrefer1231@gmail.com>
2023-11-22 23:28:50 -0300

    fix selected base

--- frontend/src/components/SelectVideo.vue
+++ frontend/src/components/SelectVideo.vue
@@ -4,7 +4,7 @@
       <div class="relative mt-2">
         <ListboxButton class="relative max-w-3xl cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
           <span class="flex items-center">
-            <span class="ml-3 block truncate">{{ selected }}</span>
+            <span class="ml-3 block truncate">{{ selected.title }}</span>
           </span>
           <span class="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
             <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
@@ -13,7 +13,7 @@
   
         <transition leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
           <ListboxOptions class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            <ListboxOption as="template" v-for="video in videos.videos" :key="video" :value="video" v-slot="{ active, selected }">
+            <ListboxOption as="template" v-for="video in videos" :key="video" :value="video" v-slot="{ active, selected }">
               <li :class="[active ? 'bg-indigo-600 text-white' : 'text-gray-900', 'relative cursor-default select-none py-2 pl-3 pr-9']">
                 <div class="flex items-center">
                   <span :class="[selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate']">{{ video.title }}</span>
@@ -38,7 +38,7 @@
   import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
   
   const videos = ref({})
-  const selected = ref(videos.value.videos)
+  const selected = ref({})
   const authStore = useAuthStore()
   const accessToken = ref(null)
   
@@ -56,12 +56,12 @@
             }
           })
           
-          videos.value = ret.data
+          videos.value = ret.data.videos
+          selected.value = videos.value[3]
         } catch (error) {
           console.error(error)
         }
       }
     }
-    console.log(selected)
   })
   </script>
