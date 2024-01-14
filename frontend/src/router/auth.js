6c8d55fa8d3ffb655028afa1d2ff6978fffb774d
Alexandre <Alexandrefer1231@gmail.com>
2024-01-13 23:49:52 -0300

    fix: render component after auth

--- frontend/src/router/auth.js
+++ frontend/src/router/auth.js
@@ -1,13 +1,10 @@
 import { router } from '@/router/index';
-import { reactive } from 'vue';
-
-const auth = reactive({})
 
 export const userAuthenticate = () => {
     router.beforeEach((to, from, next) => {
+        console.log(to)
         if (to.matched.some(record => record.meta.requiresAuth)) {
             const token = localStorage.getItem('authMembry');
-            console.log(auth)
             if (token) {
                 next()
             }
