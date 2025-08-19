3048c2bd31fc04d7bf765597c80690d0c29e808c
AlexFer-123 <alexandrefer1231@gmail.com>
2025-08-19 19:28:01 -0300

    feat: styled components base from shadcn for vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomePage.vue'
+import LoginPage from '@/views/LoginPage.vue'
+import RegisterPage from '@/views/RegisterPage.vue'
 import DashboardAdmin from '@/views/DashBoard.vue'
 import LessonEdit from '@/views/LessonEdit.vue'
 import ConfigsAdmin from '@/views/ConfigsPage.vue'
@@ -10,11 +12,25 @@ export const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/home',
       component: Home,
       name: 'HomePage',
     },
     {
+      path: '/login',
+      component: LoginPage,
+      name: 'Login',
+    },
+    {
       path: '/register',
+      component: RegisterPage,
+      name: 'Register',
+    },
+    {
+      path: '/signup',
       component: Signup,
       name: 'SignUp',
     },
