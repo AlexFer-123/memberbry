6ddb7eccd16b2fceb8efd766ab507dfadb255162
AlexFer-123 <alexandrefer1231@gmail.com>
2025-08-21 11:25:32 -0300

    fix: esling file config

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 'off'
+      }
+    }
+  ]
+}
